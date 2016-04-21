441beb0357a7549c3ad14e87e915d3b31fc79ef5
Daniil Ermakov <rubberandrobber@gmail.com>
2016-04-21 11:11:48 +0300

    added buildings

--- game/scripts/unit.gd
+++ game/scripts/unit.gd
@@ -6,6 +6,9 @@ export var actions = 1
 onready var level = get_node("../Level")
 onready var selection = get_node("../Selection")
 
+func _ready():
+	get_node("Sprite").get_node("AnimationPlayer").play("idleActive")
+
 
 func _input_event(viewport, ev, shape_idx):
 	if ev.is_action_pressed("select"):
